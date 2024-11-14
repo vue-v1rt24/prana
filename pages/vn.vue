2203d0903ec32c81259e9c76acc79503db4ac80a
Роман <rublevrus@mail.ru>
2024-11-14 18:44:49 +0300

    Правка заглушки

--- pages/vn.vue
+++ pages/vn.vue
@@ -8,16 +8,20 @@
 </template>
 
 <style lang="css" scoped>
+:global(body) {
+  overflow: hidden;
+}
+
 .zaglushka_bx {
   position: fixed;
   inset: 0;
   background-color: #030810;
-  z-index: 99;
+  z-index: 1500;
 }
 
 .zaglushka {
   position: fixed;
   inset: 0;
-  z-index: 100;
+  z-index: 1501;
 }
 </style>
